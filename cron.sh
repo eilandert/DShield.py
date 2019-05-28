4e57e8f54e2484623ae39f174b01cabdb8018e5f
eilandert <eilander@myguard.nl>
2019-05-28 22:52:33 +0200

    Update and rename logrotate.sh to cron.sh

--- cron.sh
+++ cron.sh
@@ -8,6 +8,13 @@ cd /etc/iptables/dshield
 
 #
 # Include something like this in /etc/logrotate.d/syslog
+# 
+# Crontab : 
+# 20 * * * * /etc/iptables/dshield/cron.sh
+#
+# Make sure you process the already rotated log (e.g. kern.log.1)
+#
+
 
 #/var/log/kern.log
 #{
